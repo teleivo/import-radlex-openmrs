4d213136ef230c47ed0bc5aefdb948fec5d22fa2
teleivo <mrteleivo@gmail.com>
2016-09-07 14:20:03 +0200

    update gitignore and improve scripts

--- generate-import-radlex-complete.sh
+++ generate-import-radlex-complete.sh
@@ -2,12 +2,10 @@
 
 [ -f import-radlex-complete.sql ] && rm import-radlex-complete.sql
 
-tail -n +2 complete-playbook-2_1.csv | sed -e "s/\"/'/g" > complete-playbook-2_1-sanitized.csv
-
-awk -F, '{if (NR!=1) {print "\47" sq $1 "\47," $3 sq "," sq $4 }}' complete-playbook-2_1-sanitized.csv > radlex-complete-columns.csv
+awk -vFPAT='[^,]*|"[^"]*"'']"' '{if (NR!=1) {print "\42" sq $1 "\42," $3 sq","sq $4 sq ");" }}' complete-playbook-2_1.csv > radlex-complete-columns.csv
 
 while read; do
     echo "INSERT concept_reference_term
     (concept_source_id,version,creator,date_created,uuid,code,name,description)
-    VALUES (1,'2.1',1,'2016-08-01 12:00:00','"$(uuidgen)"',"${REPLY}");" >> import-radlex-complete.sql
+    VALUES (1,\"2.1\",1,\"2016-08-01 12:00:00\",\"$(uuidgen)\",${REPLY}" >> import-radlex-complete.sql
 done < radlex-complete-columns.csv
--- generate-import-radlex-core.sh
+++ generate-import-radlex-core.sh
@@ -2,12 +2,10 @@
 
 [ -f import-radlex-core.sql ] && rm import-radlex-core.sql
 
-tail -n +2 core-playbook-2_1.csv | sed -e "s/\"/'/g" > core-playbook-2_1-sanitized.csv
-
-awk -F, '{if (NR!=1) {print "\47" sq $1 "\47," $3 sq "," sq $4 }}' core-playbook-2_1-sanitized.csv > radlex-core-columns.csv
+awk -vFPAT='[^,]*|"[^"]*"'']"' '{if (NR!=1) {print "\42" sq $1 "\42," $3 sq","sq $4 sq ");" }}' core-playbook-2_1.csv > radlex-core-columns.csv
 
 while read; do
     echo "INSERT concept_reference_term
     (concept_source_id,version,creator,date_created,uuid,code,name,description)
-    VALUES (1,'2.1',1,'2016-08-01 12:00:00','"$(uuidgen)"',"${REPLY}");" >> import-radlex-core.sql
+    VALUES (1,\"2.1\",1,\"2016-08-01 12:00:00\",\"$(uuidgen)\",${REPLY}" >> import-radlex-core.sql
 done < radlex-core-columns.csv
