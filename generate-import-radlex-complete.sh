75dacef026fe21813f4a4beb5bd036dadd3d4749
teleivo <mrteleivo@gmail.com>
2016-09-07 16:21:19 +0200

    not yet

--- generate-import-radlex-complete.sh
+++ generate-import-radlex-complete.sh
@@ -2,19 +2,26 @@
 
 [ -f import-radlex-complete.sql ] && rm import-radlex-complete.sql
 
-# Sanitize csv
-# remove header
-# remove all double quotes as some fields have them, some are missing them
-tail -n +2 complete-playbook-2_1.csv | tr -d '"' > complete-playbook-2_1-sanitized.csv
+radlex_filename="complete-playbook-2_1.csv" 
 
-# Extract columns and generate end of SQL insert satements
+# Extract columns (neglect header)
 awk -vFPAT='([^,]+|"[^"]+")' \
-   '{print "\42" $1 "\42,\42" $3 "\42,\42" $4 "\42);" }' \
-   complete-playbook-2_1-sanitized.csv > radlex-complete-columns.csv
+    '{if (NR!=1) {print $1 "," $3 "," $4 }}' \
+   $radlex_filename > radlex-complete-columns.csv
+
+# Sanitize csv
+# remove all double quotes as some fields have them, some are missing them
+cat radlex-complete-columns.csv | tr -d '"' > radlex-complete-columns-sanitized.csv
+
+# Generate final SQL insert
+awk -vFPAT='([^,]+)' \
+    '{print "\42" $1 "\42,\42" $2 "\42,\42" $3 "\42);" }' \
+   radlex-complete-columns-sanitized.csv > radlex-complete-columns-final.csv
+
 
 # Finalize SQL insert statements and output to file
 while read; do
     echo "INSERT concept_reference_term
     (concept_source_id,version,creator,date_created,uuid,code,name,description)
     VALUES (1,\"2.1\",1,\"2016-08-01 12:00:00\",\"$(uuidgen)\",${REPLY}" >> import-radlex-complete.sql
-done < radlex-complete-columns.csv
+done < radlex-complete-columns-final.csv
